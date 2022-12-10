93c1df98c9794afd01b61b41c8a50430acdc84ec
Amazingkenneth <amazingkenneth@outlook.com>
2022-12-10 22:32:01 +0800

    Passed compilation

--- src/exchange.rs
+++ src/exchange.rs
@@ -1,33 +1,73 @@
-use crate::*;
-impl EntryImage {
-    pub fn read_index() {}
-    /*async fn search() -> Result<EntryImage, Error> {
-        let fetch_entry = async {
-            let url =
-                format!("https://pokeapi.co/api/v2/pokemon-species/{}", id);
-
-            reqwest::get(&url).await?.json().await
-        };
-
-        let (entry, image): (Entry, _) =
-            futures::future::try_join(fetch_entry, Self::fetch_image(id))
-                .await?;
-
-        Ok(EntryImage {
+use crate::{Error, Memories, Stage, State};
+use iced::widget::image;
+use reqwest::Client;
+use std::fs::{self, File};
+use std::io::{Read, Write};
+use std::path::Path;
+use toml::value::Value;
+//type JoinHandle = std::thread::JoinHandle<_>;
+impl State {
+    pub async fn get_idx() -> Result<State, crate::Error> {
+        let proj_dir = directories::ProjectDirs::from("", "9B1", "Graduate").unwrap();
+        fs::create_dir_all(proj_dir.data_dir()).unwrap();
+        let location = "https://graduate-1313398930.cos.ap-guangzhou.myqcloud.com";
+        let idxurl: String = format!("{}{}", location, "/index.toml");
+        let idxdir: String = format!("{}{}", proj_dir.data_dir().display(), "/index.toml");
+        let mut contents = String::new();
+        let cli = Client::new();
+        if Path::new(&idxdir).is_file() {
+            let mut file = std::fs::File::open(&idxdir).unwrap();
+            file.read_to_string(&mut contents)
+                .expect("Cannot read the index file");
+            if let Ok(val) = contents.parse::<Value>() {
+                return Ok(State {
+                    stage: Stage::ChoosingCharacter(Default::default()),
+                    idxtable: val.as_table().ok_or(crate::Error::ParseError)?.to_owned(),
+                    client: cli,
+                    url_prefix: location.to_string(),
+                    storage: proj_dir.data_dir().to_str().unwrap().to_string(),
+                });
+            }
+        }
+        let content = cli
+            .get(&idxurl)
+            .send()
+            .await
+            .expect("Cannot send request")
+            .text()
+            .await
+            .expect("Cannot convert response to text.");
+        let mut buffer = File::create(idxdir).unwrap();
+        buffer
+            .write_all(content.as_bytes())
+            .expect("Cannot write into file.");
+        let val = content.parse::<Value>().expect("Cannot parse the content.");
+        Ok(State {
+            stage: Stage::ChoosingCharacter(Default::default()),
+            idxtable: val.as_table().ok_or(crate::Error::ParseError)?.to_owned(),
+            client: cli,
+            url_prefix: location.to_string(),
+            storage: proj_dir.data_dir().to_str().unwrap().to_string(),
         })
     }
-    async fn fetch_image(id: u16) -> Result<image::Handle, reqwest::Error> {
-        let url = format!(
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{}.png",
-            id
-        );
-
-        #[cfg(not(target_arch = "wasm32"))]
-        {
-            let bytes = reqwest::get(&url).await?.bytes().await?;
-
+    pub async fn get_image(&self, path: String) -> Result<image::Handle, reqwest::Error> {
+        let img_dir = format!("{}{}", self.storage, path);
+        if Path::new(&img_dir).is_file() {
+            return Ok(image::Handle::from_path(&img_dir));
+        }
+        async {
+            let url = format!("{}{}", self.url_prefix, path);
+            let bytes = self
+                .client
+                .get(&url)
+                .send()
+                .await
+                .expect("Cannot send request")
+                .bytes()
+                .await
+                .expect("Cannot read the image into bytes.");
             Ok(image::Handle::from_memory(bytes.as_ref().to_vec()))
         }
-
-    }*/
+        .await
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 #![allow(dead_code, unused_imports)]
 mod exchange;
-// use iced::futures;
-use iced::widget::{column, container, horizontal_space, image, row, text, vertical_space, Column};
+use iced::widget::{
+    self, column, container, horizontal_space, image, row, text, vertical_space, Column,
+};
 use iced::{window, Application, Color, Command, Element, Length, Settings, Theme};
-use toml::value::Datetime;
+use reqwest::Client;
+use toml::value::Table;
 
 const INITIAL_WIDTH: u32 = 1400;
 const INITIAL_HEIGHT: u32 = 800;
+
 pub enum LayoutDirection {
     Horizontal,
     Upright,
@@ -28,31 +31,46 @@ pub fn main() -> iced::Result {
         ..Settings::default()
     })
 }
-
 enum Memories {
     Loading,
-    // Errored,
+    Loaded(State),
+}
+
+#[derive(Clone, Debug)]
+pub struct State {
+    stage: Stage,
+    idxtable: Table,
+    client: Client,
+    url_prefix: String,
+    storage: String,
+}
+
+#[derive(Clone, Debug)]
+enum Stage {
     ChoosingCharacter(ChoosingState),
     ShowingPlots,
     Graduated,
 }
 
+#[derive(Clone, Debug, Default)]
 pub struct ChoosingState {
     chosen_character: u32,
     on_image: u32,
-    idx: Vec<EntryImage>,
+    image: Option<image::Handle>,
     // 当前图片为 idx[on_image]
-}
-pub struct EntryImage {
-    name: String,
-    description: String,
-    date: Datetime,
-    image: image::Handle,
 }
 
 #[derive(Debug)]
 enum Message {
+    Loaded(Result<State, Error>),
     ImageChanged(u32),
+}
+
+#[derive(Debug)]
+pub enum Error {
+    APIError,
+    LanguageError,
+    ParseError,
 }
 
 impl Application for Memories {
@@ -62,37 +80,97 @@ impl Application for Memories {
     type Theme = Theme;
 
     fn new(_flags: ()) -> (Memories, Command<Message>) {
-        (Memories::Loading, Command::none())
+        (
+            Memories::Loading,
+            Command::perform(State::get_idx(), Message::Loaded),
+        )
     }
+
     fn title(&self) -> String {
         let subtitle = match self {
             Memories::Loading => "加载中",
-            _ => "Whoops!",
+            Memories::Loaded(_) => "加载完毕!",
         };
         format!("{} - 有你，才是一班。", subtitle)
     }
     fn update(&mut self, message: Message) -> Command<Message> {
         match self {
-            Memories::ChoosingCharacter(s) => match message {
-                Message::ImageChanged(to) => {
-                    s.on_image = to;
+            Memories::Loading => Command::none(),
+            Memories::Loaded(state) => {
+                match &state.stage {
+                    Stage::ChoosingCharacter(chosen) => match message {
+                        Message::ImageChanged(next) => {
+                            let (mut chosen, state) = (chosen.clone(), state.clone());
+                            let img_path = state
+                                .idxtable
+                                .get("image")
+                                .expect("Cannot get item `image`")
+                                .as_array()
+                                .expect("Cannot read as an array.")[next as usize]
+                                .get("path")
+                                .expect("No path value in the item.").to_owned();
+                            *self = Memories::Loading;
+                            Command::perform(
+                                async move {
+                                    chosen.on_image = next;
+                                    chosen.image = Some(
+                                        state
+                                            .get_image(img_path.to_string())
+                                            .await
+                                            .expect("Cannot get image."),
+                                    );
+                                    Ok(state)
+                                },
+                                Message::Loaded,
+                            )
+                        }
+                        _ => {
+                            /* *self = Memories::Loading;
+                            Command::perform(, Message::ImageChanged)*/
+                            Command::none()
+                        }
+                    },
+                    _ => Command::none(),
                 }
-            },
-            _ => {}
+            }
         }
-        Command::none()
     }
     fn view(&self) -> Element<Message> {
-        let content = row![
+        match self {
+            Memories::Loading => container(
+                column![
+                    text("正在加载中……").size(40),
+                    text("有你，才是一班。").size(20)
+                ]
+                .width(Length::Shrink),
+            )
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .center_x()
+            .center_y()
+            .into(),
+            Memories::Loaded(state) => match &state.stage {
+                Stage::ChoosingCharacter(chosen) => row![
+                    match &chosen.image {
+                        Some(handle) => Element::from(image::viewer(handle.clone())),
+                        None => Element::from(text("Not able to load image.").size(40)),
+                    },
+                    Self::show_name("Class 1")
+                ]
+                .into(),
+                _ => row![Self::show_name("Not implemented")].into(),
+            },
+        }
+        /*let content = row![
             image("data/image/grade7/开学合照.jpg"),
             /*horizontal_space(Length::Units(20)),
             Self::show_profile("abc")*/
         ];
-        container(content).into()
+        container(content).into()*/
     }
 }
 impl Memories {
-    fn show_profile(title: &str) -> Column<Message> {
+    fn show_name(title: &str) -> Column<Message> {
         column![text(title).size(50)].spacing(20)
     }
 }
